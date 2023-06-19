292a311f1a7289d3a5fcba6ac06a3d5964a991fb
Gil Jeremy Ditablan <71557609+destinyarx@users.noreply.github.com>
2023-06-19 12:26:39 +0800

    Update App.jsx

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -3,31 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const user = {
+  name: 'Pablo Escobar',
+  age: 56,
+  goal: 'Learn ReactJS',
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="user-profile">
+        <h5>Name: {user.name} </h5>
+        <h5>Age: {user.age} </h5>
+        <h5>Goal: {user.goal} </h5>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
